2c3f2ef68ed7d9639eaab33a01b154a929b17076
John Murphy <chiefsmurph@gmail.com>
2015-09-04 14:32:27 -0700

    Initial commit

---
+++ autoupdategit.js
@@ -0,0 +1,17 @@
+// on each run
+// add one line from the sourcefile to targetfile
+// add, commit and push
+
+// cronjob: run node
+
+
+var sourcefile = 'angular.js';
+var targetfile = 'target.js';
+
+var fs = require('fs');
+
+//  get current number of lines of target
+
+
+
+//  get specific line of sourcefile

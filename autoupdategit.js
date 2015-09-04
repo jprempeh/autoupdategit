d9e664271ed9b5dd26f333ce9bbd30d316919307
John Murphy <chiefsmurph@gmail.com>
2015-09-04 14:46:38 -0700

    added package.json

--- autoupdategit.js
+++ autoupdategit.js
@@ -24,3 +24,22 @@ fs.createReadStream(targetfile)
 
 
 //  get specific line of sourcefile
+var get_line = function(filename, line_no, callback) {
+    fs.readFile(filename, function (err, data) {
+      if (err) throw err;
+
+      // Data is a buffer that we need to convert to a string
+      // Improvement: loop over the buffer and stop when the line is reached
+      var lines = data.toString('utf-8').split("\n");
+
+      if(+line_no > lines.length){
+        return callback('File end reached without finding line', null);
+      }
+
+      callback(null, lines[+line_no]);
+    });
+}
+
+get_line(sourcefile, 9, function(err, line){
+  console.log('The line: ' + line);
+})

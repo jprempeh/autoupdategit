76d95b5ec4e7183f297523e45a2f7fe4f234f55c
John Murphy <chiefsmurph@gmail.com>
2015-09-04 14:40:08 -0700

    gets current number of lines of targetfile

--- autoupdategit.js
+++ autoupdategit.js
@@ -11,7 +11,16 @@ var targetfile = 'target.js';
 var fs = require('fs');
 
 //  get current number of lines of target
-
+var i;
+var countLines = 0;
+fs.createReadStream(targetfile)
+  .on('data', function(chunk) {
+    for (i=0; i < chunk.length; ++i)
+      if (chunk[i] == 10) countLines++;
+  })
+  .on('end', function() {
+    console.log(countLines);
+  });
 
 
 //  get specific line of sourcefile
